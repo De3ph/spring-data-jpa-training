78f1cf43210bfc22bbad79da6268ff03ba1dccfb
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-05-08 16:27:49 +0300

    many-to-many rel

--- src/main/java/com/de3ph/spring/data/jpa/tutorial/entity/Course.java
+++ src/main/java/com/de3ph/spring/data/jpa/tutorial/entity/Course.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -40,5 +43,27 @@ public class Course {
             referencedColumnName = "teacherId"
     )
     private Teacher teacher;
+
+    @ManyToMany(
+            cascade = CascadeType.ALL
+    )
+    // @JoinTable, many-to-many için mapping tablosu yaratıyor
+    @JoinTable(
+            name="student_course_map",
+            joinColumns = @JoinColumn(
+                    name="course_id", // yaratılacak table da hangi column ismine olacak
+                    referencedColumnName = "courseId" // entitiy deki hangi column
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "student_id",
+                    referencedColumnName = "studentId"
+            )
+    )
+    private List<Student> students;
+
+    public void addStudents(Student student){
+        if (students == null) students = new ArrayList<>();
+        students.add(student);
+    }
 
 }
--- src/test/java/com/de3ph/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
+++ src/test/java/com/de3ph/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
@@ -2,6 +2,7 @@ package com.de3ph.spring.data.jpa.tutorial.repository;
 
 import com.de3ph.spring.data.jpa.tutorial.entity.Course;
 import com.de3ph.spring.data.jpa.tutorial.entity.CourseMaterial;
+import com.de3ph.spring.data.jpa.tutorial.entity.Student;
 import com.de3ph.spring.data.jpa.tutorial.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,29 @@ class CourseRepositoryTest {
                 .credit(6)
                 .teacher(teacher)
                 .build();
+
+        repository.save(course);
+    }
+
+    @Test
+    void saveCourseWithStudentAndTeacher(){
+        Teacher teacher = Teacher.builder()
+                .firstName("Lizzie")
+                .lastName("Morgan")
+                .build();
+        Student student = Student.builder()
+                .firstName("Abishek")
+                .lastName("Singh")
+                .emailId("abishek@gmail.com")
+                .build();
+        Course course = Course.builder()
+                .title("AI")
+                .credit(12)
+                .teacher(teacher)
+                .build()
+                ;
+
+        course.addStudents(student);
 
         repository.save(course);
     }
